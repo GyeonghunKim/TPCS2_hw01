ed3cc7396df99f55d9e8b23dac7968c28526e8c1
GHKim <aass98998@snu.ac.kr>
2018-09-20 12:43:50 +0900

    write functions

--- my_vector_functions.cpp
+++ my_vector_functions.cpp
@@ -13,12 +13,26 @@ std::vector<double> sum(const std::vector<double> &a, const std::vector<double> 
     }
     return z;
 }
-double dot(const std::vector<double> &a, const std::vector<double> &b){
 
+double dot(const std::vector<double> &a, const std::vector<double> &b){
+    double result = 0.0;
+    for(int i = 0; i < a.size(); ++i){
+        result += a[i]*b[i];
+    }
+    return result;
 }
-std::vector<double> linspace(const std::vector<double> &start, const std::vector<double> &stop, const int &num_points){
+std::vector<double> linspace(const double &start, const double &stop, const int &num_points){
+    std::vector<double> z(num_points);
+    for(int i = 0; i < num_points; ++i){
+        z[i] = start + i * (stop - start)/(num_points - 1);
+    }
+    return z;
 
 }
 double integrate(const std::vector<double> &x, const std::vector<double> &y){
-
+    double result = 0;
+    for(int i = 1; i < x.size(); ++i){
+        result += (y[i] + y[i-1]) * (x[i] - x[i-1]) / 2;
+    }
+    return result;
 }
