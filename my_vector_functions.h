804d8dbe34ed4984e3c0995f8bfefdf12ccec978
GHKim <aass98998@snu.ac.kr>
2018-09-20 10:54:00 +0900

    add some footnotes in header file

--- my_vector_functions.h
+++ my_vector_functions.h
@@ -7,10 +7,20 @@
 
 #include <iostream>
 #include <vector>
-
+//
+// return sum of two input vectors
 std::vector<double> sum(const std::vector<double> &a, const std::vector<double> &b);
+
+//
+//  return dot product fo two input vectors
 double dot(const std::vector<double> &a, const std::vector<double> &b);
+
+//
+//  generate vector with linear spaced value with start value, stop value and number of points
 std::vector<double> linspace(const std::vector<double> &start, const std::vector<double> &stop, const int &num_points);
+
+//
+//  return integrate data with trapezoidal methods
 double integrate(const std::vector<double> &x, const std::vector<double> &y);
 
 #endif //HW01_MY_VECTOR_FUNCTIONS_H
