412946259df667c52104fe088bc74f81d130cf99
GHKim <aass98998@snu.ac.kr>
2018-09-24 05:12:27 +0900

    make header of test functions

---
+++ test_functions.h
@@ -0,0 +1,42 @@
+//
+// Created by ghkim on 18. 9. 20.
+// test functions in my_vector_functions_h
+//
+
+#ifndef HW01_TEST_FUNCTIONS_H
+#define HW01_TEST_FUNCTIONS_H
+
+#include <iostream>
+#include <functional>
+#include <vector>
+#include "my_vector_functions.h"
+
+
+//
+// test sum()
+bool test_sum();
+
+//
+// test dot()
+bool test_dot();
+
+//
+// test linspace()
+bool test_linspace();
+
+//
+// test integral()
+bool test_integrate();
+
+//
+// function for run test
+bool run_test();
+
+//
+// run all tests
+bool run_all_tests();
+
+
+
+
+#endif //HW01_TEST_FUNCTIONS_H
