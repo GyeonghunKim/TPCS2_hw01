f499a385d52e2759ada63b0b2edb4ec18dccde6b
GHKim <aass98998@snu.ac.kr>
2018-09-24 06:20:16 +0900

    modified the method for create random sizes

--- test_functions.cpp
+++ test_functions.cpp
@@ -7,24 +7,31 @@
 #include <vector>
 
 // header for random function
-#include <cstdlib>
 #include <ctime>
+#include <random>
 
 #include "test_functions.h"
 
 // if VERBOSE true, print all vectors while run functions in test_functions.cpp
-#define VERBOSE false
+#define VERBOSE true
 
 
 bool test_sum(){
-    int vector_size = std::rand()/(RAND_MAX/100 + 1);
-    std::cout << vector_size << std::endl;
+    const unsigned int seed = time(0);
+    std::mt19937_64 rng(seed);
+    std::uniform_int_distribution<int> unii(1, 512);
+    int vector_size = unii(rng);
+    //int vector_size = std::rand()/(RAND_MAX/100 + 1);
     const std::vector<double> a = linspace(0, vector_size - 1, vector_size);
     const std::vector<double> b = linspace(0, 2 * (vector_size - 1), vector_size);
     std::vector<double> sum_result = sum(a, b);
     if(VERBOSE) {
+        std::cout << "vector size is " << vector_size << std::endl;
+        std::cout << "a: ";
         print_vector(a);
+        std::cout << "b: ";
         print_vector(b);
+        std::cout << "sum(a, b): ";
         print_vector(sum_result);
     }
     if (sum_result == linspace(0, 3 * (vector_size - 1), vector_size)){
@@ -34,6 +41,7 @@ bool test_sum(){
         return false;
     }
 }
+
 //bool test_dot()
 
 //bool test_linspace()
